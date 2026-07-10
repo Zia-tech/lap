4fe39e3237998e2e865e82b9227f3620e411db54
HariNandvv <ziafehamin123@gmail.com>
2026-07-10 15:08:39 +0530

    lap

--- src/routes/lap.routes.js
+++ src/routes/lap.routes.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const { createLap, getAllLaps, updateLap, deleteLap } = require('../controller/lap.controller');
-
+const protect = require('../middleware/auth.middleware');
 const router = express.Router();
 
-router.post('/', createLap);
+router.post('/', protect, createLap);
 router.get('/', getAllLaps);
-router.put('/:id', updateLap);
-router.delete('/:id', deleteLap);
+router.put('/:id', protect, updateLap);
+router.delete('/:id', protect, deleteLap);
 
 module.exports = router;
